2fe23aeb7ca679086f6821610a12fcb67dd6db6c
kugankuganesh <kugankuganesh@gmail.com>
2025-03-10 07:15:57 +0530

    dsgfsdgf

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
